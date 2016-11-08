fe679a6ad89c381efbe758280bac89a57c4ddaaf
Annie Sonna and James Lee <james@>
2016-11-08 11:53:42 -0800

    Add a jQuery to cross out text of a checkbox.

--- js/scripts.js
+++ js/scripts.js
@@ -16,16 +16,14 @@ function Traveler(traveler, travelPlace, travelMode, travelAccom, travelAct) {
 //   }
 // }
 
-// Traveler.prototype.generateChecklist() = function() {
+// Traveler.prototype.generateChecklist = function() {
 //   for(var index = 0; index < travelMode.length; index++)
-//     if(travelMode === "Plane")
+//     if(newtravelMode === "Plane")
 // }
 
 $(document).ready(function() {
   $("#checklist").click(function(event) {
   event.preventDefault();
-
-  alert("hello world");
 
   var newTraveler = new Traveler(newTraveler, newTravelPlace, newTravelMode, newTravelAccom, newTravelAct);
 
@@ -39,7 +37,22 @@ $(document).ready(function() {
     if(newTravelerUser.length === 0) {
       $(".multiselect").show();
     }
-
-
   });
+
+  $(function(){
+    $('input:checkbox').on('change',function(){
+        if($(this).is(':checked')){
+            $(this).siblings('span').css('text-decoration','line-through');
+        }else{
+            $(this).siblings('span').css('text-decoration','none');
+        }
 });
+});
+
+});
+
+
+// $("body").append("<div>A div element with some text</div> ");
+//
+
+// $(".divcls").append("<div><label>New Checkbox</label>: <input type='checkbox' name='checktest' value='checkbox' >Checkbox</div>");
