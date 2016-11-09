13c1730b87a039dc20356efc6df5778822f96c0e
James Lee and Amy Suzi <amy@>
2016-11-09 10:05:56 -0800

    Add functionality to append checklist.

--- js/scripts.js
+++ js/scripts.js
@@ -15,14 +15,31 @@ Traveler.prototype.travelerList = function() {
   }
 }
 
-var travelerSentencePage = [];
 
-Traveler.prototype.travelerSentence = function () {
-  for(var i = 0; i < this.travelerUser.length; i++) {
-    travelerSentencePage += "this.travelerUser[i]";
-    console.log(travelerSentencePage);
+Traveler.prototype.addMode = function() {
+  var travelModeArray = ["Plane", "Train", "Boat", "Car", "Bus"];
+  var travelModePlane = "plane";
+  var travelModeTrain = "";
+  var travelModeBoat = "";
+  var travelModeCar = "";
+  var travelModeBus = "";
+
+  for(var i = 0; i < travelModeArray.length; i++)  {
+    if (this.travelMode === travelModeArray[i]) {
+      return travelModeArray[i];
+    }
   }
 }
+
+
+// var travelerSentencePage = [];
+
+// Traveler.prototype.travelerSentence = function () {
+//   for(var i = 0; i < this.travelerUser.length; i++) {
+//     travelerSentencePage += "this.travelerUser[i]";
+//     console.log(travelerSentencePage);
+//   }
+// } <----- delete?
 
 // Front End
 
@@ -44,6 +61,7 @@ $(document).ready(function() {
     });
 
     newTraveler.travelerList();
+    newTraveler.addMode();
 
     // $(".outputs").show();
     $(".list-who").html(travelerList);
@@ -51,6 +69,13 @@ $(document).ready(function() {
     $(".list-how").text(newTraveler.travelMode);
     $(".list-staying").text(newTraveler.travelAccom);
     $(".list-activities").text(newTraveler.travelAct);
+
+    console.log(newTraveler.addMode());
+
+    if (newTraveler.addMode() === "Plane") {
+      return  $(".multiselect").append("<label><input type='checkbox' class='custom' value=''/><span> HelloWorld</span></label>");
+    };
+
   });
 
   //move to checklist.html page
@@ -70,31 +95,31 @@ $(document).ready(function() {
     });
     $(".pageOne").hide();
 
-    alert(newTraveler.travelerUser.length);
     if(newTraveler.travelerUser.length != 0) {
       $(".pageTwo").show();
     }
 
     newTraveler.travelerList();
-    newTraveler.travelerSentence();
+    // newTraveler.travelerSentence();
     // console.log(newTraveler);
     $("#sentenceUser").text(newTraveler.travelerUser + " traveling to " + newTraveler.travelPlace +" by "+ newTraveler.travelMode +  ". Accommodation: " + newTraveler.travelAccom + ". Planned activity: " + newTraveler.travelAct);
 
   });
 
-  $(function() {
-    $("#button2").submit(function() {
-      $(".multiselect").append("<label><input type='checkbox' class='custom' value=''/><span> HelloWorld</span></label>");
-    });
-  });
+  // $(function() {
+  //   $("form.multiselect").submit(function(event) {
+  //     event.preventDefault();
+  //     $(".multiselect").append("<label><input type='checkbox' class='custom' value=''/><span> HelloWorld</span></label>");
+  //   });
+  // });
 
-  $(function(){
-    $('input:checkbox').on('change',function(){
-        if($(this).is(':checked')){
-            $(this).siblings('span').css('text-decoration','line-through');
-        }else{
-            $(this).siblings('span').css('text-decoration','none');
-        }
-  });
-  });
+  // $(function(){
+  //   $('input:checkbox').on('change',function(){
+  //       if($(this).is(':checked')){
+  //           $(this).siblings('span').css('text-decoration','line-through');
+  //       }else{
+  //           $(this).siblings('span').css('text-decoration','none');
+  //       }
+  //     };
+  // };
 });
